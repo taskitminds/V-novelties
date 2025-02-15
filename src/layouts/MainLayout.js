7f14c3ab28e864a959310c282f3a286a4e145994
sanjitsriram <mahasanjit08@gmail.com>
2025-02-15 19:12:43 +0530

    Added my project to herbal repository

---
+++ src/layouts/MainLayout.js
@@ -0,0 +1,8 @@
+// The MainLayout.js is a component that acts like a template for all your pages. It usually includes things like:
+
+// Header: The top part of the page with navigation links.
+// Footer: The bottom section with extra info or links.
+// Content Area: A space for each page’s content.
+// It helps keep the page structure consistent and avoids repeating the same parts on every page.
+
+
