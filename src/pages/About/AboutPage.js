978491c1ff2e8195ebbb41710da92768a68e4087
sanjitsriram <mahasanjit08@gmail.com>
2025-02-26 07:27:23 +0530

    about

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header/Header'; // Ensure the Header component exists
 import './AboutPage.css';
+import Navbar from '../../components/Navbar/Navbar'; // Ensure the Navbar component exists
 
 const AboutUs = () => {
   return (
     <div className="about-us">
       <Header />
+      <Navbar />
         <h1 className='a'>About Us</h1>
       <section className="who-we-are">
         <h2>Who We Are</h2>
