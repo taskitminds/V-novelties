ef03078ea06649d06840b8ccb672ec12faa9df4f
sanjitsriram <mahasanjit08@gmail.com>
2025-03-19 19:15:49 +0530

    hash routes

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import HomePage from "../pages/Home/HomePage";
