252477fb141ed5e97364fb65ca435dea9e87b04d
sanjitsriram <mahasanjit08@gmail.com>
2025-03-21 19:06:36 +0530

    r and d page changes

--- src/pages/R&D/r&dpaje.js
+++ src/pages/R&D/r&dpaje.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './r&dpaje.css';
-import Image from "../../assets/images/home/r&d.webp";
 
 const RandD = () => {
   return (
@@ -8,17 +7,12 @@ const RandD = () => {
       <div className="rnd-container">
         <div className="rnd-text-container">
           <h1>INNOVATIVE SOLUTIONS OF NOVEL PRODUCTS</h1>
-          {/* <div className="leaf-decorator">🌿🌿</div> */}
           <ul>
-            <li>Vlife organize and deliver new product development with effective technical solutions.</li>
+            <li>Vlife organizes and delivers new product development with effective technical solutions.</li>
             <li>Vlife is dedicated to providing innovative solutions with flawless, quality-controlled, and assured products following global food safety management systems.</li>
-            <li>Sophisticated R&D division consists of professional doctors, skillful engineers, technicians, and well-equipped analytical facilities constantly chasing knowledge and technology transfer with the aim of manufacturing novel and ingenious products, honoring customers' preferences.</li>
-            <li>Offering world-class third-party/contract basis manufacturing based on the specifications and quantity as per customers' request.</li>
+            <li>Sophisticated R&D division with professional doctors, skillful engineers, technicians, and analytical facilities chasing knowledge and technology transfer for manufacturing novel products.</li>
+            <li>Offering world-class third-party/contract basis manufacturing as per customers' specifications and quantity requirements.</li>
           </ul>
-        </div>
-
-        <div className="rnd-image-container">
-          <img src={Image} alt="Production Facilities" />
         </div>
       </div>
     </div>
