33dd1da4fca2033c702031f16c35aeafe06e0435
yuvaprajan11 <151350946+Yuvaprajan@users.noreply.github.com>
2025-03-27 23:33:07 +0530

    final commit r&d and contact

--- src/pages/R&D/r&dpaje.js
+++ src/pages/R&D/r&dpaje.js
@@ -6,12 +6,12 @@ const RandD = () => {
     <div className="rnd-page">
       <div className="rnd-container">
         <div className="rnd-text-container">
-          <h1>INNOVATIVE SOLUTIONS OF NOVEL PRODUCTS</h1>
+          <h1>Innovative Solutions for Novel Products</h1>
           <ul>
-            <li>Vlife organizes and delivers new product development with effective technical solutions.</li>
-            <li>Vlife is dedicated to providing innovative solutions with flawless, quality-controlled, and assured products following global food safety management systems.</li>
-            <li>Sophisticated R&D division with professional doctors, skillful engineers, technicians, and analytical facilities chasing knowledge and technology transfer for manufacturing novel products.</li>
-            <li>Offering world-class third-party/contract basis manufacturing as per customers' specifications and quantity requirements.</li>
+            <li>We organize and deliver new product development with cutting-edge technical solutions.</li>
+            <li>Dedicated to innovation, we ensure high-quality, globally compliant products.</li>
+            <li>Our R&D team of expert scientists, engineers, and technicians drives technology advancement.</li>
+            <li>Offering world-class third-party manufacturing tailored to client needs.</li>
           </ul>
         </div>
       </div>
