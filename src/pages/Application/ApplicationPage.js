d8db856446950368d0bb55c9d9fd361610aa1109
yuvaprajan11 <151350946+Yuvaprajan@users.noreply.github.com>
2025-06-11 16:57:09 +0530

    Application page

--- src/pages/Application/ApplicationPage.js
+++ src/pages/Application/ApplicationPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import "./ApplicationPage.css";
-//import "../../assets/images/application/";
 
 const sectors = [
   {
@@ -14,21 +13,30 @@ const sectors = [
     ]
   },
   {
-    name: 'Dairy',
+    name: 'Personal Care',
     images: [
-      { src: 'https://t4.ftcdn.net/jpg/01/45/60/21/360_F_145602173_05uVexifBuCvWIKvsHGWNuIpPtp5ShkI.jpg', text: 'High-Quality Milk Production' },
-      { src: 'https://img.freepik.com/premium-photo/still-life-with-dairy-product_772702-4885.jpg?semt=ais_hybrid', text: 'Dairy Farm Management' },
-      { src: 'https://media.istockphoto.com/id/1297005860/photo/raw-milk-being-poured-into-container.jpg?s=612x612&w=0&k=20&c=5Xumh49_zYs9GjLkGpZXM41tS17K8M-svN9jLMv0JpE=', text: 'Innovative Dairy Products' },
-      { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQydcpjZgoRb33Z6HWMx-ohiEyxKAy1K0B57q3kHONs2C84SCa8xXWMgP9c5MLg-j0Joos&usqp=CAU', text: 'Sustainable Dairy Farming' }
+      { src: 'https://imgs.search.brave.com/qjkyEqMx_G6Ipx0uxiKzB6POnYxU-grAYDKDDhE7i4E/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzEyLzg2LzY0LzU5/LzM2MF9GXzEyODY2/NDU5MjJfNUVjY2tP/OXNiUmhzVm5vUkZX/VklHd3U0TEpyWk03/RWcuanBn', text: 'Scalp soothing and nourishing' },
+      { src: 'https://imgs.search.brave.com/T4gB1MlGTVwwZmszFgHIZOoCVpLtiBNfiq0HB--RPiY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzEwLzAxLzgxLzQz/LzM2MF9GXzEwMDE4/MTQzNzhfSmVzcmJr/M3lKdlU5VzJ5Tlhx/SmNkQ25hazRnazVz/VXouanBn', text: 'Reducing stress, Improving sleep' },
+      { src: 'https://imgs.search.brave.com/BVr7KjnY4AXBs7QBCwgBWz_gPJY__H9in4R63xCazPs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9oYWlyLXRyZWF0/bWVudC1jb25jZXB0/XzE3MzM4Ny02NTY4/LmpwZz9zZW10PWFp/c19oeWJyaWQmdz03/NDA', text: 'Boosts hair growth' },
+      { src: 'https://imgs.search.brave.com/Q37YvUHLbwNfKVs4RQqpFe-I65QvJ9bqsnNGjDa7XgI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzEwLzY3LzI2Lzgx/LzM2MF9GXzEwNjcy/NjgxNzZfRWxNSER4/NEFIMDIwbExzUGxv/NVMwZzZjaEZYc2hl/RHguanBn', text: 'Vitamin E, which helps to postpone skin ageing, hydrate the face, and stretch wrinkles.' }
     ]
   },
   {
-    name: 'Food',
+    name: 'Chicken Care',
     images: [
-      { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPZ8Hv38DtbZs2gqhTLkKT-MgbmHTHpdHVw&s', text: 'Healthy Food Production' },
-      { src: 'https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?s=612x612&w=0&k=20&c=v48RE0ZNWpMZOlSp13KdF1yFDmidorO2pZTu2Idmd3M=', text: 'Organic Ingredients' },
-      { src: 'https://images.squarespace-cdn.com/content/v1/57879a6cbebafb879f256735/1712832754805-I7IJ7FRXF629FN3PIS3O/KC310124-27.jpg', text: 'Quality Control' },
-      { src: 'https://t3.ftcdn.net/jpg/02/60/12/88/360_F_260128861_Q2ttKHoVw2VrmvItxyCVBnEyM1852MoJ.jpg', text: 'Food Safety Standards' }
+      { src: 'https://imgs.search.brave.com/hTo7THhSUvzdIDurhTKC5xBjRH4bLl4XI4bN_00Izl0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvODcy/NjQ2Mjc2L3Bob3Rv/L2NoaWNrZW4tc3Vu/c2V0LmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz02QVMxZzE5/MmR3OUM4WXpRYkJH/ZUNYT1M0OU4yTUlU/SW9XellnRUtDeEM4/PQ', text: 'For growth, weight and activeness' },
+      { src: 'https://imgs.search.brave.com/YFTIh1xGfBmwmfM3DFaMc_hxUP66ik3QhHOZXlcvpRA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTQw/NTAyMDE4L3Bob3Rv/L2JhYnktY2hpY2tl/bnMtYXQtdGhlLWZh/cm0tZHJpbmtpbmct/d2F0ZXIuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPVJTbE9M/MDRFVllpSU54SlFr/bWQ2V2x5QkFfNFRs/TmpGU2xjaEh4eTF2/Q0E9', text: 'Increases resistance of diseases' },
+      { src: 'https://imgs.search.brave.com/JfKzN7XlsHS5DBle2P0nBDlBTqHiGE28QHkSiEPa-XM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTc3/OTYwMzEwL3Bob3Rv/L2NoaWNrZW5zLWVh/dGluZy1pbi1iYWNr/Z3JvdW5kLW9mLW11/bHRpLWNvbG9yZWQt/ZWdncy5qcGc_Yj0x/JnM9NjEyeDYxMiZ3/PTAmaz0yMCZjPUpp/N2hUdEw5eHF0dUtq/YmctbnZKNGMydkJV/LWxMZ1dCZEV3c2Vo/SzlKVnc9', text: 'Helps prevent problems of thin shell and broken egg and controls lameness' },
+      { src: 'https://imgs.search.brave.com/WbG-W5FjhTyKp_jrU_LZeYdwV05DF7xyRAIjWrFCbKk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/Y3JlYXRlLnZpc3Rh/LmNvbS9hcGkvbWVk/aWEvc21hbGwvMTEz/MDQ2NTUyL3N0b2Nr/LXBob3RvLWNoaWNr/ZW5zLW9uLXRyYWRp/dGlvbmFsLWZyZWUt/cmFuZ2UtcG91bHRy/eS1mYXJt', text: 'Increases weight gain and metabolism' }
+    ]
+  },
+  {
+    name: 'Cow Care',
+    images: [
+      { src: 'https://imgs.search.brave.com/1Lzcx2DLHGcAeJPNH9ayaUXy-ZkTesNAE09HcuD31wg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/Y3JlYXRlLnZpc3Rh/LmNvbS9hcGkvbWVk/aWEvc21hbGwvMjgy/MTcyNjEvc3RvY2st/cGhvdG8tY293LWFu/ZC1jYWxm', text: 'Prevents vitamins and minerals deficiency' },
+      { src: 'https://imgs.search.brave.com/yePwQf5YXBw5Tiba3UTf9cU8rRE-Ga-m6uqAZFr7eVo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9mYXJtLWZyZXNo/LW1pbGstcHJvZmVz/c2lvbmFsLWNhcHR1/cmVzLW1pbGstYmVp/bmctY29sbGVjdGVk/LWZyb20tZGFpcnkt/Y293cy1mYXJtLWVt/cGhhc2l6aW5nLWZy/ZXNobmVzcy1xdS1h/aS1nZW5lcmF0ZWQt/aWxsdXN0cmF0aW9u/Xzg2NjY2My0yNTA5/OS5qcGc_c2VtdD1h/aXNfaHlicmlkJnc9/NzQw', text: 'Assured High and quality milk production' },
+      { src: 'https://imgs.search.brave.com/I5bkhQK4PxZYSOBwXF0DRbyXCSSu8-D04Xqw5dhO-1k/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9jb3dzLWVhdGlu/Zy1mYXJtaW5nLWNh/dHRsZS1kYWlyeS1m/YXJtLWFncmljdWx0/dXJlLWdyb3d0aC1m/b29kLXByb2R1Y3Rp/b24tbmF0dXJlLWVh/dC1oZXJkLWh1bmdy/eS1hbmltYWxzLXdp/dGgtZmVlZC1jb3Vu/dHJ5c2lkZS1saXZl/c3RvY2stbGlmZXN0/eWxlLWluZHVzdHJ5/XzU5MDQ2NC0xOTYz/ODYuanBnP3NlbXQ9/YWlzX2h5YnJpZCZ3/PTc0MA', text: 'Better udder health' },
+      { src: 'https://imgs.search.brave.com/KuA5HCJ9v-7htmR5W242e7Wtlpfi54ZSLsOzkZjg0aw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDIwMzQz/MDQuanBn', text: 'Supreme SNF, Protein and taste content in milk' }
     ]
   },
 ];
